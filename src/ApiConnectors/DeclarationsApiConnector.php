219b0988fb7f4f1e6543ada85000bda9e4358797
Marcel Frankruijter <marcelfrankruijter@gmail.com>
2026-02-24 11:38:42 +0100

    fix: more inconsistencies from twinfield

--- src/ApiConnectors/DeclarationsApiConnector.php
+++ src/ApiConnectors/DeclarationsApiConnector.php
@@ -3,6 +3,7 @@
 namespace PhpTwinfield\ApiConnectors;
 
 use PhpTwinfield\Mappers\DocumentMapper;
+use stdClass;
 
 class DeclarationsApiConnector extends BaseApiConnector
 {
@@ -27,14 +28,18 @@ class DeclarationsApiConnector extends BaseApiConnector
     {
         $response = $this->getDeclarationService()->vatReturnXbrl($documentId);
 
-        return $response->VatReturn->any ?? null;
+        $vatReturn = $this->getVatReturnResponse($response);
+
+        return $vatReturn?->any ?? null;
     }
 
     public function icpReturnXBrl($documentId): string|null
     {
         $response = $this->getDeclarationService()->icpReturnXbrl($documentId);
 
-        return $response->VatReturn->any ?? null;
+        $vatReturn = $this->getVatReturnResponse($response);
+
+        return $vatReturn?->any ?? null;
     }
 
     public function summaries($officeCode, $declarationYear = null): array
@@ -67,13 +72,15 @@ class DeclarationsApiConnector extends BaseApiConnector
         return $this->processDeclarations($response);
     }
 
-    private function processDeclarations(\stdClass $response): array
+    private function processDeclarations(stdClass $response): array
     {
-        if (!isset($response->VatReturn->DeclarationSummary)) {
+        $vatReturn = $this->getVatReturnResponse($response);
+
+        if (!is_object($vatReturn) || !isset($vatReturn->DeclarationSummary)) {
             return [];
         }
 
-        $declarations = $response->VatReturn->DeclarationSummary;
+        $declarations = $vatReturn->DeclarationSummary;
 
         // If there are multiple declarations, map them all
         if (is_array($declarations)) {
@@ -84,6 +91,13 @@ class DeclarationsApiConnector extends BaseApiConnector
 
         // With XML, if there is only one declaration, it is not an array
         return [DocumentMapper::map($declarations)];
+    }
+
+    private function getVatReturnResponse(stdClass $response): ?stdClass
+    {
+        $vatReturn = $response->vatReturn ?? $response->VatReturn ?? null;
+
+        return is_object($vatReturn) ? $vatReturn : null;
     }
 
     public function setApproved($officeCode, $documentId): bool
